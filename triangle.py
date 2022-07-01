c482fd8a1651ab6f6b41826b3ab74248b516a8b2
jacob.m.koll <jacob.m.koll@gmail.com>
2022-06-30 21:20:51 -0500

    fully converted code to class via inheriting tk directly.

--- triangle.py
+++ triangle.py
@@ -1,23 +1,27 @@
 from tkinter import *
 import random
 
-random.seed(random.random())
-root = Tk()
-root.resizable(False, False)
-root.title('Sierpinski\'s Triangle Demo')
-c_width, c_height = 1000, 1000
-canvas = Canvas(root, width=c_width, height=c_height, bg='black')
-canvas.pack()
+class Sierpinski(Tk):
+    def __init__(self, parent, iterations):
+        Tk.__init__(self, parent)
+        self.c_width = 1000
+        self.c_height = 1000
+        self.iterations = min(100000, iterations)
+        self.resizable(False, False)
+        self.title('Sierpinski\'s Triangle Demo')
+        self.initialize()
 
-def plot_point(x, y):
-    canvas.create_oval(x, y, x+1, y+1, outline='white')
-    return {'x':x, 'y':y}
+    def initialize(self):
+        self.canvas = Canvas(self, width=self.c_width, height=self.c_height, bg='black')
+        self.canvas.pack()
+        self.t = {1:self.plot_point(self.c_width/2, self.c_height-950),
+                  2:self.plot_point(self.c_width-950, self.c_height-50),
+                  3:self.plot_point(self.c_width-50, self.c_height-50)}
+        self.simulate(self.iterations)
 
-class Sierpinski:
-    def __init__(self):
-        self.t = {1:plot_point(c_width/2, c_height-950),
-                  2:plot_point(c_width-950, c_height-50),
-                  3:plot_point(c_width-50, c_height-50)}
+    def plot_point(self, x, y):
+        self.canvas.create_oval(x, y, x+1, y+1, outline='white')
+        return {'x':x, 'y':y}
 
     def intercept(self, x, tline):
         m = (tline['y2'] - tline['y1'])/(tline['x2'] - tline['x1'])
@@ -33,22 +37,12 @@ class Sierpinski:
             tline = {'x1':self.t[1]['x'], 'y1':self.t[1]['y'], 'x2':self.t[3]['x'], 'y2':self.t[3]['y']}
         rp_y = random.randint(self.intercept(rp_x, tline), self.t[2]['y'])
 
-        p = plot_point(rp_x, rp_y)
+        p = self.plot_point(rp_x, rp_y)
         for point in range(n_points):
             rtp = self.t[random.randint(1,3)]
-            mp = plot_point((p['x']+rtp['x'])/2,(p['y']+rtp['y'])/2)
+            mp = self.plot_point((p['x']+rtp['x'])/2,(p['y']+rtp['y'])/2)
             p = mp
 
-triangle = Sierpinski()
-iterations = 10000
-triangle.simulate(iterations)
+triangle = Sierpinski(None, 10000)
+triangle.mainloop()
 
-# This is the worst way to update the canvas. I will update later to keep track of each point and draw and erase as the slider moves.
-# def slider_changed(event):
-#     canvas.delete("all")
-#     triangle.simulate(slider.get())
-
-# slider = Scale(root,from_=0,to=iterations,length=c_width,orient='horizontal',command=slider_changed)
-# slider.pack()
-
-root.mainloop()
